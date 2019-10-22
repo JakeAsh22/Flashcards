8d6af5b6889c74c03ce7c66f01229006f5d7bb4f
Jake Ash and Jordan Safford <jordan@>
2019-10-22 13:39:34 -0700

    Will make a flashcard website, with 20 different flash cards.

---
+++ js/scripts.js
@@ -0,0 +1,92 @@
+$(document).ready(function() {
+  $("#card1").click(function(){
+    $(".front1").toggle();
+    $(".back1").toggle();
+
+  });
+  $("#card2").click(function() {
+    $(".front2").toggle();
+    $(".back2").toggle();
+  });
+  $("#card3").click(function(){
+    $(".front3").toggle();
+    $(".back3").toggle();
+
+  });
+  $("#card4").click(function() {
+    $(".front4").toggle();
+    $(".back4").toggle();
+  });
+  $("#card5").click(function(){
+    $(".front5").toggle();
+    $(".back5").toggle();
+
+  });
+  $("#card6").click(function() {
+    $(".front6").toggle();
+    $(".back6").toggle();
+  });
+  $("#card7").click(function(){
+    $(".front7").toggle();
+    $(".back7").toggle();
+
+  });
+  $("#card8").click(function() {
+    $(".front8").toggle();
+    $(".back8").toggle();
+  });
+  $("#card9").click(function(){
+    $(".front9").toggle();
+    $(".back9").toggle();
+
+  });
+  $("#card10").click(function() {
+    $(".front10").toggle();
+    $(".back10").toggle();
+  });
+  $("#card11").click(function(){
+    $(".front11").toggle();
+    $(".back11").toggle();
+
+  });
+  $("#card12").click(function() {
+    $(".front12").toggle();
+    $(".back12").toggle();
+  });
+  $("#card13").click(function(){
+    $(".front13").toggle();
+    $(".back13").toggle();
+
+  });
+  $("#card14").click(function() {
+    $(".front14").toggle();
+    $(".back14").toggle();
+  });
+  $("#card15").click(function(){
+    $(".front15").toggle();
+    $(".back15").toggle();
+
+  });
+  $("#card16").click(function() {
+    $(".front16").toggle();
+    $(".back16").toggle();
+  });
+  $("#card17").click(function(){
+    $(".front17").toggle();
+    $(".back17").toggle();
+
+  });
+  $("#card18").click(function() {
+    $(".front18").toggle();
+    $(".back18").toggle();
+  });
+  $("#card19").click(function(){
+    $(".front19").toggle();
+    $(".back19").toggle();
+
+  });
+  $("#card20").click(function() {
+    $(".front20").toggle();
+    $(".back20").toggle();
+  });
+});
